15c944de93086134306eef69e1360db016fb97f0
AliMurtazaJaffery <murtaza@connect.hku.hk>
2021-02-25 18:55:30 +0800

    Update Work Experience

--- src/Pages/Work.tsx
+++ src/Pages/Work.tsx
@@ -1,16 +1,31 @@
 import firstCode from './WorkImages/firstcode.png'
+import CSHKU from './WorkImages/CSHKU.png'
+import risksis from './WorkImages/risksis.png'
+import FSI from './WorkImages/FSI.png'
 import {Flex,Image,Heading,Box,Text} from '@chakra-ui/react'
+
+type workProps={
+  image:string,
+  position: React.ReactNode,
+  company:React.ReactNode,
+  timePeriod:React.ReactNode,
+}
+const WorkExperience:React.FunctionComponent<workProps> = ({position,company, image,timePeriod})=>(
+  <Box align="center" mb="45px" w="45%" >          
+    <Image src={image} boxSize={["60px","80px"]}  width={["180px","200px"]}/>
+    <Heading mt="20px" fontSize={["sm","md","xl","xl"]}>{company}</Heading>
+    <Text fontSize={["xs","sm","md","md"]}>{position}</Text>
+    <Text fontSize={["xs","sm","md","md"]}>{timePeriod}</Text>
+  </Box>
+)
 
 export default function Work(){
   return(
     <>
-      <Flex align="space-between" w="40%" mx="30%">
-      <Box align="center" >          
-          <Image src={firstCode} boxSize="100px"  width="240px"/>
-          <Heading mt="20px" fontSize="xl">First Code Academy</Heading>
-          <Text>Summer tech Instructor</Text>
-          <Text>(May 2019 - August 2019)</Text>
-        </Box>
+      <Flex justify="space-between" w={["80%","80%","60%","40%"]} mx={["10%","10%","20%","30%"]} wrap="wrap" mb="80px">
+        <WorkExperience image={risksis} position="Software Developer (Part Time)" company="Risksis Technology Limited" timePeriod="March(2021) - Present"/>
+        <WorkExperience image={FSI} position="Software Developer (Part Time)" company="Foundation For Shared Impact" timePeriod="January(2021) - Present"/>
+        <WorkExperience image={firstCode} position="Summer tech Instructor" company="First Code Academy" timePeriod="(May 2019 - August 2019)" /> 
       </Flex>
     </>
   )
