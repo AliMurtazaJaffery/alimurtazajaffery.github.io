4c8b87e0e05d2e78a2241385aa756155ba33f6dc
AliMurtazaJaffery <murtaza@connect.hku.hk>
2021-07-10 15:16:44 +0800

    add progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,13 @@ import Navbar from "../components/Navbar/Navbar"
 import SEO from "../components/SEO/SEO"
 import Footer from "../components/Footer/Footer"
 import type { AppProps } from 'next/app'
+import NextNprogress from 'nextjs-progressbar';
 import '../../public/style.css'
 
 function MyApp({ Component, pageProps }:AppProps) {
   return (
     <>
+    <NextNprogress height={6}/>
     <SEO/>
     <ChakraProvider>
     	<Header/>
